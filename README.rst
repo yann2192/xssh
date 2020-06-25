4d0292ba807fa34ddcafc72b1fd6353bd0f9d2cf
Yann GUIBET <yannguibet@gmail.com>
2020-06-25 09:21:53 +0200

    Add setup.py & README

---
+++ README.rst
@@ -0,0 +1,25 @@
+Requirements
+============
+
+- tmux
+
+Usage
+=====
+
+Basic Usage
+::
+
+    $ xssh host1 host2 host3
+
+Features
+--------
+
+Quit
+::
+
+    (Cmd) !q
+
+Get display from the first host
+::
+
+    (Cmd) !1
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="xssh",
+    version="0.1",
+    packages=setuptools.find_packages(),
+    scripts=["bin/xssh"],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=["libtmux==0.8.2"],
+)
